3f9a14cbf491a7aa6dfd49e14efd6c0bb6f1c945
Jinseo Sung <jinseosung15@gmail.com>
2024-01-24 10:11:57 +0100

    Jest Mock Function

--- src/components/tests/Validation.test.js
+++ src/components/tests/Validation.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Validation from "../Validation";
 
 describe("Validation component", () => {
@@ -12,5 +13,27 @@ describe("Validation component", () => {
     render(<Validation nextLevel="cartStep" />);
     const btnCart = screen.getByRole("button", { name: /ajouter/i });
     expect(btnCart).toBeInTheDocument();
+  });
+
+  test("Setter setStep invoked only once to move to the confirmation step", async () => {
+    const goToConfirmationStep = jest.fn();
+    const user = userEvent.setup();
+    render(
+      <Validation setStep={goToConfirmationStep} nextLevel="confirmationStep" />
+    );
+
+    const btnConfirmation = screen.getByRole("button", { name: /valider/i });
+    await user.click(btnConfirmation);
+    expect(goToConfirmationStep).toHaveBeenCalledTimes(1);
+  });
+
+  test("Setter setStep invoked only once to move to the cart step", async () => {
+    const goToConfirmationStep = jest.fn();
+    const user = userEvent.setup();
+    render(<Validation setStep={goToConfirmationStep} nextLevel="cartStep" />);
+
+    const btnCart = screen.getByRole("button", { name: /ajouter/i });
+    await user.click(btnCart);
+    expect(goToConfirmationStep).toHaveBeenCalledTimes(1);
   });
 });
